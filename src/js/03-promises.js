c95fb3c961d8dd45a9a29f59a72d0e4bac7557d4
Morzat Devourer <judith.zamora.go@gmail.com>
2023-06-08 14:15:51 -0600

    5th commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,45 +1,31 @@
-
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector('.form');
 
-form.addEventListener('submit', createPromises);
+form.addEventListener('submit', onFormSubmit);
 
-function createPromises(event) {
+function onFormSubmit(event) {
   event.preventDefault();
 
   const delayInput = document.querySelector('input[name="delay"]');
   const stepInput = document.querySelector('input[name="step"]');
   const amountInput = document.querySelector('input[name="amount"]');
 
-  const delay = Number(delayInput.value);
+  let delay = Number(delayInput.value);
   const step = Number(stepInput.value);
   const amount = Number(amountInput.value);
 
-  const promises = [];
-
-  for (let i = 0; i < amount; i++) {
-    const position = i + 1;
-    const promiseDelay = delay + i * step;
-
-    const promise = createPromise(position, promiseDelay);
-    promises.push(promise);
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay).then(onSuccess).catch(onError);
+    delay += step;
+  }
+}
+  function onSuccess({ position, delay }) {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   }
 
-  Promise.all(promises)
-    .then((results) => {
-      results.forEach(({ position, delay }) => {
-        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      });
-    })
-    .catch((results) => {
-      results.forEach(({ position, delay }) => {
-        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
-    });
-
-  delayInput.value = '';
-  stepInput.value = '';
-  amountInput.value = '';
+  function onError({ position, delay }) {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 }
 
 function createPromise(position, delay) {
@@ -54,5 +40,4 @@ function createPromise(position, delay) {
       }
     }, delay);
   });
-}
-
+};
